343c45d03b712fa7ccd0e689b0e365454cbc9cdd
ThomasHolley <holley.t2203@gmail.com>
2023-03-30 21:49:17 +0200

    initialisation du projet

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Base de donnée de prod
+$host = 'gironderwu866.mysql.db';
+$dbname = 'gironde';
+$username = 'gironderwu866';
+$password = 'vSDqFH2Sjv5P';
+ */
+
+// Base de donnée de dev
+$host = 'localhost';
+$dbname = 'giftlink_db';
+$username = 'thg';
+$password = '8*6/Fcdv2203';
